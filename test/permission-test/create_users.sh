056dfa267d7f565474977021f96ac6a11a44ec2d
soumya-pn <soumya.p.n@hpe.com>
2020-02-11 05:22:31 +0000

    Added permission test scripts version 0.2

--- test/permission-test/create_users.sh
+++ test/permission-test/create_users.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-for i in $( cat users.txt ); do
+for i in $( echo “user1 user2 user3 user4”  ); do
     useradd $i
-    echo "user $i added successfully!"
-    echo $i:$i"123" | chpasswd
-    echo "Password for user $i changed successfully"
+    if [[ $? == 0 ]]; then
+    	echo "user $i added successfully!"
+    	echo $i:$i"123" | chpasswd
+    	echo "Password for user $i changed successfully"
+    fi
 done
-#user1 = read 1 "users.txt"
-#user2 = read 2 "users.txt"
-#user3 = read 3 "users.txt"
 usermod -g user1 user2
 usermod -G user1 user3
