99e2ca60ecf4ce706927fd06dcfcbea87b7bbbd0
BA-Ramya <ramyaba@gmail.com>
2020-05-19 21:59:35 -0700

    Fix for #18 and #19 - Build script modifications:  - Optimize build script to not install on default directories  - Come up with build scripts for centos/rhel flavors.    (The build script has been qualified on centos and can be    used with rhel as well (subject to availability of dependent rpms.) Updated files:         modified:   CMakeLists.txt         modified:   src/rpc/CMakeLists.txt         modified:   third-party/build.sh         new file:   third-party/centos_build.sh         new file:   third-party/nvmm_build_centos.sh

--- third-party/centos_build.sh
+++ third-party/centos_build.sh
@@ -27,30 +27,28 @@
 #
 # See https://spdx.org/licenses/BSD-3-Clause
 #
-
 #
 #This script assumes you have NOPASSWD access to sudo command
 #If it doesnt work, encourage you to copy paste the command and workaround,
 #Or you can edit sudoers file to give NOPASSWD access.
-#Note : This works only on ubuntu.
 #
+CURRENT_DIR=`pwd`
 BUILD_DIR="build"
 LIB_DIR="$BUILD_DIR/lib"
+INCLUDE_DIR="$BUILD_DIR/include"
 BIN_DIR="$BUILD_DIR/bin"
 OBJ_DIR="$BUILD_DIR/obj"
-INCLUDE_DIR="$BUILD_DIR/include"
-CURRENT_DIR=`pwd`
 PMIX_OBJ_DIR="$CURRENT_DIR/$BUILD_DIR"
 ABS_BUILD_DIR="$CURRENT_DIR/$BUILD_DIR"
+
+
 #creating build directory if not present
 if [ ! -d "$BUILD_DIR" ]; then
   mkdir $BUILD_DIR
 fi
-
 if [ ! -d "$INCLUDE_DIR" ]; then
   mkdir $INCLUDE_DIR
 fi
-
 if [ ! -d "$LIB_DIR" ]; then
   mkdir $LIB_DIR
 fi
@@ -58,7 +56,6 @@ fi
 if [ ! -d "$BIN_DIR" ]; then
   mkdir $BIN_DIR
 fi
-
 if [ ! -d "$OBJ_DIR" ]; then
   mkdir $OBJ_DIR
 fi
@@ -66,27 +63,62 @@ fi
 #Download all dependency source files
 ./download.sh
 
-#Function for checking ubuntu release version
-function get_ubuntu_release(){
-    cat /etc/issue | tr "\\n" " "
-}
+echo "Installing required RPMs"
 
-#Install dependency packages
-sudo apt-get install --assume-yes build-essential autoconf libtool pkg-config
+sudo yum install --assumeyes gcc gcc-c++ kernel-devel make cmake autoconf-archive glibc python python-devel automake libtool doxygen
+sudo yum install --assumeyes libevent cmake xsltproc xmlto autoconf-archive 
 if [[ $? > 0 ]]
 then
-        echo "apt-get install failed, exiting..."
         exit 1
 fi
 
+# OpenMPI rpms require libevent-devel rpms. If this package is not available as part of RHEL repos, we fetch it from centos repo
+OS=`grep -m1 "ID=" /etc/os-release | sed 's/"//g' | sed 's/ID=//g' `
+arch=`lscpu | grep Architecture | awk '{print $2}'`
+libevent_ver=`yum list libevent | tail -n 1 | awk '{print $2}'`
+os_ver=`grep VERSION_ID /etc/os-release |  awk -F '=' '{print $2}' | sed 's/"//g'`
+os_major_version=`echo $os_ver | awk -F '.' '{print $1}'`
+sudo yum list libevent-devel
+exit_status=$?
+case $OS in
+
+"rhel")
+	sudo yum install --assumeyes  http://mirror.centos.org/centos/${os_major_version}/os/${arch}/Packages/libevent-devel-${libevent_ver}.${arch}.rpm
+	;;
+"centos")
+	sudo yum install --assumeyes  libevent-devel
+	;;
+esac
+
+echo $BUILD_DIR
+
+echo "Installing boost-1.63.0"
+if [[ ! -e boost_1_63_0 ]]; then
+	wget https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.tar.gz
+	tar -zxvf boost_1_63_0.tar.gz
+fi
+
+cd boost_1_63_0/
+./bootstrap.sh
+sudo ./b2 install --prefix=$ABS_BUILD_DIR
+if [[ $? > 0 ]]
+then
+        exit 1
+fi
+
+export BOOST_ROOT=$ABS_BUILD_DIR
+
+echo "Finished installing required RPMS and boost library"
+
+cd ..
+
 #Install grpc
 cd grpc
-
 #Update the third party modules
 git submodule update --init
 
-#For parallel compilation, make -j is selected. For systems with limited DRAM, avoid using -j option for all make commands
-make  -j
+#calling make - For parallel compilation, make -j is selected. For systems with limited DRAM, avoid using -j option for all make commands
+make -j
 if [[ $? > 0 ]]
 then
         echo "Make of grpc failed.. exit..."
@@ -105,34 +137,22 @@ fi
 cd ./third_party/protobuf
 
 if [ -e Makefile ]; then
-        sudo make install prefix=$ABS_BUILD_DIR
+       sudo make install prefix=$ABS_BUILD_DIR
         if [[ $? > 0 ]]
-        then
+      then
              echo "Make install of protobuf failed.. exit..."
              exit 1
         fi
 fi
 
-cd $CURRENT_DIR
-#PMIX and PRRTE Build
-case $(get_ubuntu_release) in
-    *16.04*)
-        sudo apt-get install --assume-yes libpthread-stubs0-dev libevent-dev flex hwloc libhwloc-dev libhwloc-plugins
-        wget https://github.com/libevent/libevent/archive/release-2.1.6-beta.tar.gz
-        tar zxvf release-2.1.6-beta.tar.gz
-        cd libevent-release-2.1.6-beta/
-        ./autogen.sh; ./configure; make; sudo make install
-        cd $CURRENT_DIR
-        ;;
-    *)
-        sudo apt-get install --assume-yes libpthread-stubs0-dev libevent-2.1-6 libevent-dev flex hwloc libhwloc-dev libhwloc-plugins
-esac
+#Creating soft links for gRPC libraries
+cd ../../../$LIB_DIR
 
-if [[ $? > 0 ]]
-then
-        echo "apt-get install failed, exiting..."
-        exit 1
-fi
+ln -s libgrpc++.so.1.17.0-dev libgrpc++.so.1
+
+ln -s libgrpc++_reflection.so.1.17.0-dev libgrpc++_reflection.so.1
+
+cd $CURRENT_DIR
 
 cd pmix-3.0.2
 ./autogen.pl --force
@@ -144,7 +164,7 @@ cd $CURRENT_DIR
 
 cd openmpi-4.0.1
 OPENMPI_OBJ_DIR="$CURRENT_DIR/$BUILD_DIR"
-./configure --prefix="$CURRENT_DIR/$BUILD_DIR" --with-pmix="$CURRENT_DIR/$BUILD_DIR" --with-pmi-libdir="$CURRENT_DIR/$BUILD_DIR"
+./configure --prefix="$CURRENT_DIR/$BUILD_DIR" --with-pmix="$CURRENT_DIR/$BUILD_DIR" --with-pmi-libdir="$CURRENT_DIR/$BUILD_DIR" --with-libevent=external
 make -j
 make install
 if [[ $? > 0 ]]
@@ -154,9 +174,33 @@ then
 fi
 cd $CURRENT_DIR
 
+#Build and Install libfabric
+INSTALLDIR="$CURRENT_DIR/build"
+
+#Libfabric installation
+cd libfabric/
+
+./autogen.sh 
+./configure; make
+if [[ $? > 0 ]]
+then
+        echo "Make of libfabric failed.. exit..."
+        exit 1
+fi
+
+make install prefix=$INSTALLDIR
+cd $CURRENT_DIR
+
+
 #Install nvmm
+if [[ ! -e nvmm ]]; then
+# Install nvmm 
+	tar -zxvf nvmm.tar.gz
+fi
+
+cp ./nvmm_build_centos.sh nvmm/
 cd ./nvmm
-../nvmm_build.sh
+./nvmm_build_redhat.sh
 
 if [[ $? > 0 ]]
 then
@@ -168,31 +212,14 @@ cp -r include/nvmm ../$INCLUDE_DIR/.
 
 cd $CURRENT_DIR
 
-#Build and Install libfabric
-INSTALLDIR="$CURRENT_DIR/build"
-
-
-cd libfabric/
-
-./autogen.sh ;
-./configure; make -j
-if [[ $? > 0 ]]
-then
-        echo "Make of libfabric failed.. exit..."
-        exit 1
-fi
-
-make install prefix=$INSTALLDIR
-
-cd $CURRENT_DIR
 
 #build and install radixtree in third-party/build
 #radixtree is required to build fammetadata library.
 tar -zxvf radixtree.tar.gz
 # Currently radixtree build doesn't provide compiler option -  "-std=c++11" by default.But without this radixtree build fails.
 # Following line can be omitted if that option is added in the radixtree github.
-sed -i '22i\set(CMAKE_CXX_FLAGS "-std=c++11")\' radixtree/CMakeLists.txt 
 
+sed -i '22i\set(CMAKE_CXX_FLAGS "-std=c++11")\' radixtree/CMakeLists.txt
 cd radixtree; mkdir build; cd build
 cmake ..; make
 
@@ -206,3 +233,6 @@ mkdir ../../$INCLUDE_DIR/radixtree
 cp ../*.h  ../../$INCLUDE_DIR/radixtree
 
 cd $CURRENT_DIR
+
+
+
