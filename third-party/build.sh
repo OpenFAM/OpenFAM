4c51a1cd0357417d5b3ab718bafc65724c1f065e
BA-Ramya <62128713+BA-Ramya@users.noreply.github.com>
2020-07-14 19:27:48 +0530

    Fix #57: Create YAML configuration files for OpenFAM (#58)
    * Fix #57: Create YAML configuration files parsing wrapper 
    
     - Created header files to access yaml config files

--- third-party/build.sh
+++ third-party/build.sh
@@ -85,10 +85,10 @@ case $OS in
 		fi
 		case $(get_ubuntu_release) in
 		    *16.04*)
-			sudo apt-get install --assume-yes libpthread-stubs0-dev libevent-dev flex hwloc libhwloc-dev libhwloc-plugins
+			sudo apt-get install --assume-yes libpthread-stubs0-dev libevent-dev flex hwloc libhwloc-dev libhwloc-plugins libyaml-cpp-dev
 			;;
 		    *)
-			sudo apt-get install --assume-yes libpthread-stubs0-dev libevent-2.1-6 libevent-dev flex hwloc libhwloc-dev libhwloc-plugins
+			sudo apt-get install --assume-yes libpthread-stubs0-dev libevent-2.1-6 libevent-dev flex hwloc libhwloc-dev libhwloc-plugins libyaml-cpp-dev
 		esac
 		if [[ $? > 0 ]]
 		then
@@ -99,7 +99,7 @@ case $OS in
 		;;
 	"rhel" | "centos")
 		sudo yum install --assumeyes gcc gcc-c++ kernel-devel make cmake autoconf-archive glibc python python-devel automake libtool doxygen
-		sudo yum install --assumeyes libevent cmake xmlto libevent-devel
+		sudo yum install --assumeyes libevent cmake xmlto libevent-devel yaml-cpp
 		if [[ $? > 0 ]]
 		then
 			exit 1
