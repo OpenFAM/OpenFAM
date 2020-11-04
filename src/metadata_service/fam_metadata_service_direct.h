9ea60e7252f4ca8f069523cb00ca209f069a2760
BA-Ramya <62128713+BA-Ramya@users.noreply.github.com>
2020-11-04 16:35:28 +0530

    Region Spanning Code Changes (#89)
    * Region Spanning Code Changes
     - Added configuration parameters to enable or disable region spanning
     - Code corrections related to region and data item permissions

--- src/metadata_service/fam_metadata_service_direct.h
+++ src/metadata_service/fam_metadata_service_direct.h
@@ -59,7 +59,8 @@ namespace metadata {
 
 class Fam_Metadata_Service_Direct : public Fam_Metadata_Service {
   public:
-    void Start(bool use_meta_reg);
+    void Start(bool use_meta_reg, bool enable_region_spanning,
+               size_t size_per_memoryserver);
     void Stop();
 
     void reset_profile();
