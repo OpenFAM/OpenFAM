5b2c49b045b14c6262503c1e5bf4c9274f5ffce5
Mashood Abdulla <mashood.abdulla@hpe.com>
2021-06-15 03:44:43 -0700

    Updated VERSION number and NEWS.md

--- src/fam-api/fam.cpp
+++ src/fam-api/fam.cpp
@@ -1,6 +1,6 @@
 /*
  * fam.cpp
- * Copyright (c) 2020-2021 Hewlett Packard Enterprise Development, LP. All
+ * Copyright (c) 2019-2021 Hewlett Packard Enterprise Development, LP. All
  * rights reserved. Redistribution and use in source and binary forms, with or
  * without modification, are permitted provided that the following conditions
  * are met:
