3fa27a9f2aa5c5c3403f527ea6f8059c363712a7
Cynara Justine <94096751+cynara-justine@users.noreply.github.com>
2022-03-04 18:26:25 +0530

    Fixed #178 : Improved 'connect failed' error msgs (#184)

--- src/memory_service/fam_memory_service_client.cpp
+++ src/memory_service/fam_memory_service_client.cpp
@@ -102,8 +102,9 @@ Fam_Memory_Service_Client::Fam_Memory_Service_Client(const char *name,
     /** sending a start signal to server **/
     ::grpc::Status status = stub->signal_start(&ctx, req, &res);
     if (!status.ok()) {
-        throw Memory_Service_Exception(FAM_ERR_RPC,
-                                       (status.error_message()).c_str());
+        message << "Fam Memory Service: "<<status.error_message().c_str()<<":"<<name_s;
+        throw  Memory_Service_Exception(FAM_ERR_RPC, message.str().c_str());
+
     }
 
     Fam_Memory_Type memory_type = (Fam_Memory_Type)res.memorytype();
