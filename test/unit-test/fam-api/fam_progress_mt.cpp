ac83c26fc8a6e8ed6baec48c28ab6c368f8a563f
Cynara Justine <cynara.justine@hpe.com>
2022-05-12 02:23:24 -0700

    Addressed review comments https://github.com/OpenFAM/OpenFAM/pull/216

--- test/unit-test/fam-api/fam_progress_mt.cpp
+++ test/unit-test/fam-api/fam_progress_mt.cpp
@@ -30,16 +30,16 @@
  * fam_progress operations for multithreaded model.
  */
 
+#include <atomic>
 #include <fam/fam.h>
 #include <fam/fam_exception.h>
-
 #include <iostream>
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
-#include "common/fam_test_config.h"
+//#include "common/fam_test_config.h"
 
 using namespace std;
 using namespace openfam;
@@ -55,9 +55,9 @@ int rc;
 #define REGION_SIZE (2000 * 1024 * NUM_THREADS)
 #define NUM_CONTEXTS 8
 #define REGION_PERM 0777
-#define NUM_IO_ITERATIONS 50
+#define NUM_IO_ITERATIONS 100
 fam_context *ctx[NUM_CONTEXTS];
-int activethreads = NUM_THREADS;
+std::atomic<int> activethreads{NUM_THREADS};
 pthread_barrier_t barrier;
 typedef struct {
     Fam_Descriptor *item;
@@ -68,13 +68,33 @@ typedef struct {
 
 void *thr_check_fam_progress(void *arg) {
     uint64_t progress = 0;
+    cpu_set_t cpuset;
+    // the CPU we whant to use
+    int cpu;
+    cpu = 0;
+
+    CPU_ZERO(&cpuset);     // clears the cpuset
+    CPU_SET(cpu, &cpuset); // set CPU 2 on cpuset
+
+    /*
+     * cpu affinity for the calling thread
+     * first parameter is the pid, 0 = calling thread
+     * second parameter is the size of your cpuset
+     * third param is the cpuset in which your thread will be
+     * placed. Each bit represents a CPU
+     */
+    sched_setaffinity(0, sizeof(cpuset), &cpuset);
+
     pthread_barrier_wait(&barrier);
     while (activethreads != 0) {
         for (int i = 0; i < NUM_CONTEXTS; i++) {
             try {
                 progress = ctx[i]->fam_progress();
-                cout << "I/Os in Progress for context" << i << " is "
-                     << progress << endl;
+                pthread_yield();
+                if (progress != 0) {
+                    cout << "I/Os in Progress for context" << i << " is "
+                         << progress << endl;
+                }
             } catch (Fam_Exception &e) {
                 cout << "Exception caught while invoking fam_progress()"
                      << endl;
@@ -91,22 +111,50 @@ void *thrd_fam_context_mt_with_fam_progress(void *arg) {
     ValueInfo *addInfo = (ValueInfo *)arg;
     Fam_Descriptor *item = addInfo->item;
     int i = 0;
+    cpu_set_t cpuset;
+    int node0[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+                   14, 15, 16, 17, 18, 19, 40, 41, 42, 43, 44, 45, 46, 47,
+                   48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59};
+    int node1[] = {20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+                   34, 35, 36, 37, 38, 39, 60, 61, 62, 63, 64, 65, 66, 67,
+                   68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79};
+    // the CPU we whant to use
+    int cpu;
+    if (addInfo->tid % 2 == 0)
+        cpu = node0[addInfo->tid];
+    else
+        cpu = node1[addInfo->tid];
+
+    CPU_ZERO(&cpuset);     // clears the cpuset
+    CPU_SET(cpu, &cpuset); // set CPU 2 on cpuset
+
+    /*
+     * cpu affinity for the calling thread
+     * first parameter is the pid, 0 = calling thread
+     * second parameter is the size of your cpuset
+     * third param is the cpuset in which your thread will be
+     * placed. Each bit represents a CPU
+     */
+    sched_setaffinity(0, sizeof(cpuset), &cpuset);
     i = addInfo->tid % NUM_CONTEXTS;
 
     pthread_barrier_wait(&barrier);
     try {
         for (int j = 0; j < NUM_IO_ITERATIONS; j++) {
-            int *local1 = (int *)malloc(1000000);
-            int *local2 = (int *)malloc(1000000);
+            int *local1 = (int *)malloc((i + 1) * 1000000);
+            int *local2 = (int *)malloc((i + 1) * 1000000);
             ctx[i]->fam_get_nonblocking(local1, item, 500, (i + 1) * 100000);
             ctx[i]->fam_get_nonblocking(local2, item, 10, (i + 1) * 100000);
         }
-        ctx[i]->fam_quiet();
+        if (((addInfo->tid - 1) / NUM_CONTEXTS) == 0) {
+            ctx[i]->fam_quiet();
+        }
 
     } catch (Fam_Exception &e) {
         printf("fam API failed: %d: %s\n", e.fam_error(), e.fam_error_msg());
     }
-    --activethreads;
+
+    activethreads.fetch_add(-1);
 
     pthread_exit(NULL);
 }
@@ -114,8 +162,12 @@ void *thrd_fam_context_mt_with_fam_progress(void *arg) {
 int main(int argc, char **argv) {
     int ret = 0;
     my_fam = new fam();
-    init_fam_options(&fam_opts);
     fam_opts.famThreadModel = strdup("FAM_THREAD_MULTIPLE");
+
+    Fam_Options *fam_opts = (Fam_Options *)malloc(sizeof(Fam_Options));
+    memset((void *)fam_opts, 0, sizeof(Fam_Options));
+    // assume that no specific options are needed by the implementation
+    fam_opts->runtime = strdup("NONE");
 
     Fam_Descriptor *item;
     pthread_t thr[TOTAL_NUM_THREADS];
@@ -125,7 +177,8 @@ int main(int argc, char **argv) {
     const char *dataItem = "first";
 
     try {
-        my_fam->fam_initialize("default", &fam_opts);
+        my_fam->fam_initialize("default", fam_opts);
+        printf("FAM initialized\n");
     } catch (Fam_Exception &e) {
         printf("FAM Initialization failed: %s\n", e.fam_error_msg());
         my_fam->fam_abort(-1); // abort the program
@@ -133,7 +186,7 @@ int main(int argc, char **argv) {
         // so we must terminate with the same value
         return -1;
     }
-    testRegionStr = get_uniq_str("test", my_fam);
+    testRegionStr = "testfp";
     try {
         testRegionDesc = my_fam->fam_create_region(testRegionStr, REGION_SIZE,
                                                    REGION_PERM, NULL);
@@ -157,10 +210,11 @@ int main(int argc, char **argv) {
                e.fam_error(), e.fam_error_msg());
         return -1;
     }
-    if (item != NULL)
-        printf("Successully completed fam_allocate\n");
-    int local[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    my_fam->fam_put_nonblocking(local, item, 6 * sizeof(int), 10 * sizeof(int));
+    if (item != NULL) {
+        int local[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        my_fam->fam_put_nonblocking(local, item, 6 * sizeof(int),
+                                    10 * sizeof(int));
+    }
     try {
         for (i = 0; i < NUM_CONTEXTS; i++) {
             ctx[i] = my_fam->fam_context_open();
@@ -175,7 +229,6 @@ int main(int argc, char **argv) {
         cout << "Error in barrier initialization" << endl;
         return -1;
     }
-    // Create a thread for checking fam_progress - The 0 th thread
     if ((rc = pthread_create(&thr[0], NULL, thr_check_fam_progress, NULL))) {
         fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
         return -1;
@@ -193,11 +246,12 @@ int main(int argc, char **argv) {
         }
     }
 
-    pthread_barrier_destroy(&barrier);
-
     for (int j = 0; j < 33; j++) {
         pthread_join(thr[j], NULL);
     }
+
+    pthread_barrier_destroy(&barrier);
+
     try {
 
         for (i = 0; i < NUM_CONTEXTS; i++) {
@@ -224,9 +278,9 @@ int main(int argc, char **argv) {
         ret = -1;
     }
     delete testRegionDesc;
-    free((void *)testRegionStr);
     try {
         my_fam->fam_finalize("default");
+        printf("FAM finalized\n");
     } catch (Fam_Exception &e) {
         printf("FAM Finalization failed: %s\n", e.fam_error_msg());
         my_fam->fam_abort(-1); // abort the program
